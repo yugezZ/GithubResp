0d11a68ef0dae479648020f04110ce74a87b1ea9
LiuJQ <jieqiangaini@163.com>
2021-02-24 17:24:30 +0800

    提交myClass

---
+++ Module01/src/main/java/com/example/Module01/myClass.java
@@ -0,0 +1,10 @@
+package com.example.Module01;
+
+/**
+ * @ Author: LiuJieQiang
+ * @ Date: 2021/2/24 -02 -24 -17:24
+ * @ Description: com.example.Module01
+ * @ version: 1.0
+ */
+public class myClass {
+}
