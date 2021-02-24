96865d5b6e68e0f509c946a582b29d49346576ab
LiuJQ <jieqiangaini@163.com>
2021-02-24 17:34:00 +0800

    提交person类

---
+++ Module01/src/main/java/com/example/Module01/person.java
@@ -0,0 +1,11 @@
+package com.example.Module01;
+
+/**
+ * @ Author: LiuJieQiang
+ * @ Date: 2021/2/24 -02 -24 -17:33
+ * @ Description: com.example.Module01
+ * @ version: 1.0
+ */
+public class person {
+
+}
